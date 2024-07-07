349b3fbef92603ba72c07bcab772d43e416a3acd
kiarashfz <kiarashfz.fazli@gmail.com>
2024-07-07 20:06:36 +0330

    feat: migrations added

--- pkg/adapters/storage/setup.go
+++ pkg/adapters/storage/setup.go
@@ -3,16 +3,24 @@ package storage
 import (
 	"fmt"
 	"server/config"
+	"server/pkg/adapters/storage/entities"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-func NewPostgresGormConnection(dbConfig config.DBConfig) (*gorm.DB, error) {
+func NewPostgresGormConnection(dbConfig config.DB) (*gorm.DB, error) {
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable",
 		dbConfig.Host, dbConfig.User, dbConfig.Pass, dbConfig.DBName, dbConfig.Port)
 	return gorm.Open(postgres.Open(dsn), &gorm.Config{})
 }
 
-func Migrate(db *gorm.DB) {
+func Migrate(db *gorm.DB) error {
+	migrator := db.Migrator()
+
+	err := migrator.AutoMigrate(&entities.User{})
+	if err != nil {
+		return err
+	}
+	return nil
 }
