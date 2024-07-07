3faa4f544b7d9cb5e7008596bd63d787dfc6ecb5
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2024-07-07 12:43:42 +0330

    Set up Viper configuration, updated to PostgreSQL connection, and added initial app structure

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,44 @@
 package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"server/config"
+	"server/service"
+
+	http_server "server/api/http"
+)
+
+var configPath = flag.String("config", "", "configuration path")
+
+func main() {
+	cfg := readConfig()
+
+	app, err := service.NewAppContainer(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	http_server.Run(cfg.Server, app)
+}
+
+func readConfig() config.Config {
+	flag.Parse()
+
+	if cfgPathEnv := os.Getenv("APP_CONFIG_PATH"); len(cfgPathEnv) > 0 {
+		*configPath = cfgPathEnv
+	}
+
+	if len(*configPath) == 0 {
+		log.Fatal("configuration file not found")
+	}
+
+	cfg, err := config.ReadStandard(*configPath)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return cfg
+}
