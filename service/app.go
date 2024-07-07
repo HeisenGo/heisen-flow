a454130aba30c308ef74fc104005e113e49720c0
kiarashfz <kiarashfz.fazli@gmail.com>
2024-07-07 19:56:25 +0330

    feat: auth service registered

--- service/app.go
+++ service/app.go
@@ -3,14 +3,16 @@ package service
 import (
 	"log"
 	"server/config"
+	"server/internal/user"
 	"server/pkg/adapters/storage"
 
 	"gorm.io/gorm"
 )
 
 type AppContainer struct {
-	cfg    config.Config
-	dbConn *gorm.DB
+	cfg         config.Config
+	dbConn      *gorm.DB
+	authService *AuthService
 }
 
 func NewAppContainer(cfg config.Config) (*AppContainer, error) {
@@ -19,7 +21,12 @@ func NewAppContainer(cfg config.Config) (*AppContainer, error) {
 	}
 
 	app.mustInitDB()
-	storage.Migrate(app.dbConn)
+	err := storage.Migrate(app.dbConn)
+	if err != nil {
+		log.Fatal("Migration failed: ", err)
+	}
+
+	app.setAuthService()
 
 	return app, nil
 }
@@ -39,4 +46,18 @@ func (a *AppContainer) mustInitDB() {
 	}
 
 	a.dbConn = db
+}
+
+func (a *AppContainer) AuthService() *AuthService {
+	return a.authService
+}
+
+func (a *AppContainer) setAuthService() {
+	if a.authService != nil {
+		return
+	}
+
+	a.authService = NewAuthService(user.NewOps(storage.NewUserRepo(a.dbConn)), []byte(a.cfg.Server.TokenSecret),
+		a.cfg.Server.TokenExpMinutes,
+		a.cfg.Server.RefreshTokenExpMinutes)
 }
