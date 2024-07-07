6e3691597c5006e80811f2bfdac7db779a6df33d
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2024-07-07 14:01:35 +0330

    Set up Viper configuration, updated to PostgreSQL connection, and added initial app structure

--- api/http/setup.go
+++ api/http/setup.go
@@ -1,1 +1,21 @@
 package http
+
+import (
+	"fmt"
+	"log"
+	"server/config"
+	"server/service"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func Run(cfg config.Server, app *service.AppContainer) {
+	fiberApp := fiber.New()
+
+	// register global routes
+
+	// registering users APIs
+
+	// run server
+	log.Fatal(fiberApp.Listen(fmt.Sprintf("%s:%d", cfg.Host, cfg.HttpPort)))
+}
--- config/read.go
+++ config/read.go
@@ -1,1 +1,45 @@
 package config
+
+import (
+	"path/filepath"
+
+	"github.com/spf13/viper"
+)
+
+func ReadStandard(cfgPath string) (Config, error) {
+	var cfg Config
+
+	fullAbsPath, err := absPath(cfgPath)
+	if err != nil {
+		return cfg, err
+	}
+
+	viper.SetConfigFile(fullAbsPath)
+	viper.SetConfigType("yaml")
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		return cfg, err
+	}
+
+	if err := viper.Unmarshal(&cfg); err != nil {
+		return cfg, err
+	}
+
+	return cfg, nil
+}
+
+func absPath(cfgPath string) (string, error) {
+	if !filepath.IsAbs(cfgPath) {
+		return filepath.Abs(cfgPath)
+	}
+	return cfgPath, nil
+}
+
+func MustReadStandard(configPath string) Config {
+	cfg, err := ReadStandard(configPath)
+	if err != nil {
+		panic(err)
+	}
+	return cfg
+}
