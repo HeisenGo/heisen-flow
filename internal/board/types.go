a5e650da74a6fcc57e84ba6653b0babcf9691ee5
Neda Hn <z.hosseini.7@gmail.com>
2024-07-08 22:39:24 +0330

    feat: get board by id

--- internal/board/types.go
+++ internal/board/types.go
@@ -17,15 +17,16 @@ const (
 )
 
 var (
-	ErrWrongType   = errors.New("wrong type for board")
-	ErrInvalidName = errors.New("invalid board name: must be 1-100 characters long and can only contain alphanumeric characters, spaces, hyphens, underscores, and periods")
-	ErrWrongBoardTime  = errors.New("wrong board time")
+	ErrWrongType      = errors.New("wrong type for board")
+	ErrInvalidName    = errors.New("invalid board name: must be 1-100 characters long and can only contain alphanumeric characters, spaces, hyphens, underscores, and periods")
+	ErrWrongBoardTime = errors.New("wrong board time")
+	ErrBoardNotFound = errors.New("board not found")
 )
 
 type Repo interface {
 	GetUserBoards(ctx context.Context, userID uuid.UUID, limit, offset uint) ([]Board, uint, error)
 	Insert(ctx context.Context, board *Board) error
-}
+	GetByID(ctx context.Context, id uuid.UUID) (*Board, error)}
 
 type Board struct {
 	// UpdatedAt time.Time
@@ -38,7 +39,7 @@ type Board struct {
 
 func ValidateBoardName(name string) error {
 	var validBoardName = regexp.MustCompile(`^[a-zA-Z0-9 ._-]{1,100}$`)
-	if !validBoardName.MatchString(name){
+	if !validBoardName.MatchString(name) {
 		return ErrInvalidName
 	}
 	return nil
