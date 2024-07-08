6ae7cc750056df5fcd4a3362b4759a6e820dfbc0
Neda Hn <z.hosseini.7@gmail.com>
2024-07-08 12:55:11 +0330

    feat: create board service

--- service/board.go
+++ service/board.go
@@ -4,23 +4,67 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	userboard "server/internal/user_board"
+	"server/internal/board"
+	u "server/internal/user"
+	userboardrole "server/internal/user_board_role"
 	"server/pkg/rbac"
+
+	"github.com/google/uuid"
 )
 
 // BoardService handles board-related operations
 type BoardService struct {
-	userBoardOps *userboard.Ops
+	userOps          *u.Ops
+	boardOps         *board.Ops
+	userBoardRoleOps *userboardrole.Ops
 }
 
 // NewBoardService creates a new BoardService
-func NewBoardService(userBoardOps *userboard.Ops) *BoardService {
-	return &BoardService{userBoardOps: userBoardOps}
+func NewBoardService(userOps *u.Ops, boardOps *board.Ops, userBoardOps *userboardrole.Ops) *BoardService {
+	return &BoardService{userOps: userOps, boardOps: boardOps, userBoardRoleOps: userBoardOps}
+}
+
+func (s *BoardService) GetUserBoards(ctx context.Context, userID uuid.UUID, page, pageSize uint) ([]board.Board, uint, error) {
+	user, err := s.userOps.GetUserByID(ctx, userID)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	if user == nil {
+		return nil, 0, u.ErrUserNotFound
+	}
+
+	return s.boardOps.UserBoards(ctx, userID, page, pageSize)
+}
+
+func (s *BoardService) CreateBoard(ctx context.Context, b *board.Board, ub *userboardrole.UserBoardRole) error {
+	user, err := s.userOps.GetUserByID(ctx, ub.UserID)
+	if err != nil {
+		return err
+	}
+
+	if user == nil {
+		return u.ErrUserNotFound
+	}
+
+	err = s.boardOps.Create(ctx, b)
+	if err != nil {
+		return err
+	}
+
+	ub.BoardID = b.ID
+	ub.Role = rbac.RoleOwner
+	err = s.userBoardRoleOps.SetUserBoardRole(ctx, ub)
+	if err != nil {
+		return err
+	}
+	return nil
+	// return errors.New("test error") -> for testing transaction
 }
 
 // CreateTask creates a new task in a board
 func (s *BoardService) CreateTask(ctx context.Context, userID, boardID string, taskDetails map[string]interface{}) error {
-	role, err := s.userBoardOps.GetUserBoardRole(ctx, userID, boardID)
+	role, err := s.userBoardRoleOps.GetUserBoardRole(ctx, userID, boardID)
 	if err != nil {
 		return err
 	}
@@ -35,7 +79,7 @@ func (s *BoardService) CreateTask(ctx context.Context, userID, boardID string, t
 
 // MoveTask moves a task to a different column
 func (s *BoardService) MoveTask(ctx context.Context, userID, boardID, taskID, newColumnID string) error {
-	role, err := s.userBoardOps.GetUserBoardRole(ctx, userID, boardID)
+	role, err := s.userBoardRoleOps.GetUserBoardRole(ctx, userID, boardID)
 	if err != nil {
 		return err
 	}
@@ -63,7 +107,7 @@ func (s *BoardService) MoveTask(ctx context.Context, userID, boardID, taskID, ne
 
 // AddColumn adds a new column to the board
 func (s *BoardService) AddColumn(ctx context.Context, userID, boardID string, columnDetails map[string]interface{}) error {
-	role, err := s.userBoardOps.GetUserBoardRole(ctx, userID, boardID)
+	role, err := s.userBoardRoleOps.GetUserBoardRole(ctx, userID, boardID)
 	if err != nil {
 		return err
 	}
@@ -79,7 +123,7 @@ func (s *BoardService) AddColumn(ctx context.Context, userID, boardID string, co
 
 // InviteUser invites a user to the board
 func (s *BoardService) InviteUser(ctx context.Context, inviterID, boardID, inviteeID string, role rbac.Role) error {
-	inviterRole, err := s.userBoardOps.GetUserBoardRole(ctx, inviterID, boardID)
+	inviterRole, err := s.userBoardRoleOps.GetUserBoardRole(ctx, inviterID, boardID)
 	if err != nil {
 		return err
 	}
