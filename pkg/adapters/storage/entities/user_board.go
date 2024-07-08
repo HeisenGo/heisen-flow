e2d84f201c4dddbf2700cd9eb0618934617cb9c4
Neda Hn <z.hosseini.7@gmail.com>
2024-07-08 08:40:11 +0330

    fix: userboard uuid and its relationship with user and board

--- pkg/adapters/storage/entities/user_board.go
+++ pkg/adapters/storage/entities/user_board.go
@@ -2,14 +2,21 @@ package entities
 
 import (
 	"server/pkg/rbac"
+	"time"
 
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 // UserBoardRole struct: Represents the relationship between a user, a board, and the user's role on that board.
 type UserBoardRole struct {
-	gorm.Model // to do use UUID
-	UserID     string
-	BoardID    string
-	Role       rbac.Role
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+	ID        uuid.UUID      `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
+	UserID    uuid.UUID
+	User      User `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	BoardID   uuid.UUID `gorm:"index"`
+	Board     Board `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	Role      rbac.Role
 }
