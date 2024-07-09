a8314380ceedd2b595c47b1b82bdd88c43ba2369
Neda Hn <z.hosseini.7@gmail.com>
2024-07-09 12:48:07 +0330

    feat: task related entities and its relations added

--- pkg/adapters/storage/entities/board.go
+++ pkg/adapters/storage/entities/board.go
@@ -8,11 +8,14 @@ import (
 )
 
 type Board struct {
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	DeletedAt gorm.DeletedAt `gorm:"index"`
 	ID        uuid.UUID      `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
 	Name      string         `gorm:"index"`
 	Type      string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+	// Relationships
 	//Users     []User `gorm:"many2many:user_board_roles;constraint:OnDelete:CASCADE;"`
+	Tasks      []Task `gorm:"foreignKey:BoardID"`
+	UserBoardRoles []UserBoardRole `gorm:"foreignKey:BoardID"`
 }
