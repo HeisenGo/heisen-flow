60dea995abc65a48fbffded1cdc05024dcc933d8
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2024-07-10 13:37:29 +0330

    fix: added service functions

--- service/column.go
+++ service/column.go
@@ -3,6 +3,7 @@ package service
 import (
 	"context"
 	"server/internal/column"
+	"server/pkg/adapters/storage/entities"
 
 	"github.com/google/uuid"
 )
@@ -15,7 +16,7 @@ func NewColumnService(colOps *column.Ops) *ColumnService {
 	return &ColumnService{colOps: colOps}
 }
 
-func (s *ColumnService) CreateColumn(ctx context.Context, name string, boardID uuid.UUID, order uint) (*column.Column, error) {
+func (s *ColumnService) CreateColumn(ctx context.Context, name string, boardID uuid.UUID, order uint) (*entities.Column, error) {
 	col := &column.Column{
 		ID:      uuid.New(),
 		Name:    name,
@@ -27,13 +28,55 @@ func (s *ColumnService) CreateColumn(ctx context.Context, name string, boardID u
 		return nil, err
 	}
 
-	return col, nil
+	return &entities.Column{
+		ID:      col.ID,
+		Name:    col.Name,
+		BoardID: col.BoardID,
+		Order:   col.Order,
+	}, nil
 }
 
-func (s *ColumnService) GetColumnByID(ctx context.Context, id uuid.UUID) (*column.Column, error) {
+func (s *ColumnService) GetColumnByID(ctx context.Context, id uuid.UUID) (*entities.Column, error) {
 	col, err := s.colOps.GetColumnByID(ctx, id)
 	if err != nil {
 		return nil, err
 	}
-	return col, nil
+	return &entities.Column{
+		ID:      col.ID,
+		Name:    col.Name,
+		BoardID: col.BoardID,
+		Order:   col.Order,
+	}, nil
+}
+
+func (s *ColumnService) GetMaxOrderForBoard(ctx context.Context, boardID uuid.UUID) (uint, error) {
+	return s.colOps.GetMaxOrderForBoard(ctx, boardID)
+}
+
+func (s *ColumnService) CreateColumns(ctx context.Context, columns []entities.Column) ([]entities.Column, error) {
+	colModels := make([]column.Column, len(columns))
+	for i, col := range columns {
+		colModels[i] = column.Column{
+			ID:      col.ID,
+			Name:    col.Name,
+			BoardID: col.BoardID,
+			Order:   col.Order,
+		}
+	}
+
+	createdCols, err := s.colOps.CreateColumns(ctx, colModels)
+	if err != nil {
+		return nil, err
+	}
+
+	createdEntities := make([]entities.Column, len(createdCols))
+	for i, col := range createdCols {
+		createdEntities[i] = entities.Column{
+			ID:      col.ID,
+			Name:    col.Name,
+			BoardID: col.BoardID,
+			Order:   col.Order,
+		}
+	}
+	return createdEntities, nil
 }
