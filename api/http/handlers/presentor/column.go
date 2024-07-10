489ad6bbdbca597c8db1b647666abcff766e1f97
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2024-07-10 13:39:22 +0330

    feat: added presenter

--- api/http/handlers/presentor/column.go
+++ api/http/handlers/presentor/column.go
@@ -1,35 +1,55 @@
 package presenter
 
 import (
-	"server/internal/column"
+	"server/pkg/adapters/storage/entities"
 
 	"github.com/google/uuid"
 )
 
-type Column struct {
-	ID      uuid.UUID `json:"id"`
-	Name    string    `json:"name"`
-	BoardID uuid.UUID `json:"board_id"`
-	Order   uint      `json:"order"`
+type CreateColumnsRequest struct {
+	BoardID uuid.UUID          `json:"board_id"`
+	Columns []CreateColumnItem `json:"columns"`
 }
 
-func ColumnToResponse(c *column.Column) *Column {
-	if c == nil {
-		return nil
-	}
-	return &Column{
-		ID:      c.ID,
-		Name:    c.Name,
-		BoardID: c.BoardID,
-		Order:   c.Order,
-	}
+type CreateColumnItem struct {
+	Name string `json:"name"`
 }
 
-func RequestToColumn(name string, boardID uuid.UUID, order uint) *column.Column {
-	return &column.Column{
-		ID:      uuid.New(),
-		Name:    name,
-		BoardID: boardID,
-		Order:   order,
+type CreateColumnsResponse struct {
+	Data    []ColumnResponseItem `json:"data"`
+	Message string               `json:"message"`
+}
+
+type ColumnResponseItem struct {
+	ID    uuid.UUID `json:"id"`
+	Name  string    `json:"name"`
+	Order uint      `json:"order"`
+}
+
+func CreateColumnsRequestToEntities(req CreateColumnsRequest, maxOrder uint) []entities.Column {
+	columns := make([]entities.Column, len(req.Columns))
+	for i, col := range req.Columns {
+		columns[i] = entities.Column{
+			ID:      uuid.New(),
+			Name:    col.Name,
+			BoardID: req.BoardID,
+			Order:   maxOrder + uint(i) + 1,
+		}
+	}
+	return columns
+}
+
+func EntitiesToCreateColumnsResponse(columns []entities.Column) CreateColumnsResponse {
+	respItems := make([]ColumnResponseItem, len(columns))
+	for i, col := range columns {
+		respItems[i] = ColumnResponseItem{
+			ID:    col.ID,
+			Name:  col.Name,
+			Order: col.Order,
+		}
+	}
+	return CreateColumnsResponse{
+		Data:    respItems,
+		Message: "Columns successfully created.",
 	}
 }
