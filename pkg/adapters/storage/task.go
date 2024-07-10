d2a72e913107cadc06253e3737492a568b488e80
Neda Hn <z.hosseini.7@gmail.com>
2024-07-10 10:14:00 +0330

    feat: create task query considering dependencies

--- pkg/adapters/storage/task.go
+++ pkg/adapters/storage/task.go
@@ -21,17 +21,52 @@ func NewTaskRepo(db *gorm.DB) task.Repo {
 }
 
 func (r *taskRepo) Insert(ctx context.Context, t *task.Task) error {
-	// check dependency circle [does the ids exist]
-	//
-	// check roles
 	taskEntity := mappers.TaskDomainToEntity(t)
+
 	if err := r.db.WithContext(ctx).Save(&taskEntity).Error; err != nil {
 		return err
 	}
 
 	t.ID = taskEntity.ID
+	if len(t.DependsOnTaskIDs) > 0 {
+		var existingTasks []entities.Task
+		if err := r.db.Where("id IN ?", t.DependsOnTaskIDs).Find(&existingTasks).Error; err != nil {
+			return task.ErrFailedToFindDependsOnTasks
+		}
+
+		if len(existingTasks) != len(t.DependsOnTaskIDs) {
+			return task.ErrFailedToFindDependsOnTasks
+		}
+
+		var taskDependencies []entities.TaskDependency
+		for _, dependencyID := range t.DependsOnTaskIDs {
+			taskDependencies = append(taskDependencies, entities.TaskDependency{
+				DependentTaskID:  taskEntity.ID,
+				DependencyTaskID: dependencyID,
+			})
+		}
+		if err := r.db.Create(&taskDependencies).Error; err != nil {
+			return task.ErrFailedToCreateTaskDependencies
+		}
+
+	}
 	return nil
 }
+
+// func (r *taskRepo) AddDependency(ctx context.Context, t *task.Task) error {
+// 	// check dependency circle [does the ids exist]
+// 	for _, tID := range t.DependsOnTaskIDs {
+// 		if r.CheckCircularDependency(t.ID, tID) {
+// 			return task.ErrCircularDependency
+// 		}
+// 	}
+// 	if err := r.db.WithContext(ctx).Model(&entities.Task{ID: taskID}).
+// 		Association("DependsOn").
+// 		Append(&entities.Task{ID: dependencyID}); err != nil {
+// 		return err
+// 	}
+// 	return nil
+// }
 
 // using dfs
 
@@ -61,4 +96,3 @@ func (r *taskRepo) CheckCircularDependency(taskID, dependencyID uuid.UUID) bool 
 	dependentCircle := dfs(dependencyID)
 	return dependentCircle
 }
-
