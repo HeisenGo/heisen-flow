859a47a02762bda1b2e94d66d9a38c1fca430fab
kiarashfz <kiarashfz.fazli@gmail.com>
2024-07-10 18:57:23 +0330

    feat: Validators

--- api/http/handlers/presentor/common.go
+++ api/http/handlers/presentor/common.go
@@ -2,6 +2,7 @@ package presenter
 
 import (
 	"bytes"
+	"github.com/go-playground/validator/v10"
 	"math"
 	"time"
 )
@@ -43,4 +44,42 @@ func NewPagination[T any](data []T, page, pageSize, total uint) *PaginationRespo
 		TotalPages: totalPages,
 		Data:       data,
 	}
+}
+
+// This is the validator instance
+// for more information see: https://github.com/go-playground/validator
+var validate = validator.New()
+
+type XValidator struct {
+	validator *validator.Validate
+}
+
+func (v XValidator) Validate(data interface{}) []Response {
+	var validationErrors []Response
+
+	errs := validate.Struct(data)
+	if errs != nil {
+		for _, err := range errs.(validator.ValidationErrors) {
+			// In this case data object is actually holding the User struct
+			var elem Response
+
+			elem.Success = false
+			elem.Error = err.Error() // Export field value
+
+			validationErrors = append(validationErrors, elem)
+		}
+	}
+
+	return validationErrors
+}
+
+var appValidator *XValidator
+
+func GetValidator() *XValidator {
+	if appValidator == nil {
+		appValidator = &XValidator{
+			validator: validate,
+		}
+	}
+	return appValidator
 }
