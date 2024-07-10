6dacb611b1b62b884e0e4a3fa7890ab5cddb1be6
Neda Hn <z.hosseini.7@gmail.com>
2024-07-10 15:33:01 +0330

    fix: updated presenter of task

--- api/http/handlers/presentor/task.go
+++ api/http/handlers/presentor/task.go
@@ -2,23 +2,23 @@ package presenter
 
 import (
 	"server/internal/task"
-	"time"
 
 	"github.com/google/uuid"
 )
 
 type UserTask struct {
-	ID             uuid.UUID `json:"task_id"`
-	StartAt        Timestamp `json:"start_at"`
-	EndAt          Timestamp `json:"end_at"`
-	AssigneeUserID uuid.UUID `json:"assignee_user_id"`
-	Title          string    `json:"title"`
-	Description    string    `json:"desc"`
-	StoryPoint     uint      `json:"story_point"`
+	ID             uuid.UUID  `json:"task_id"`
+	Board_id       uuid.UUID  `json:"board_id"`
+	StartAt        *Timestamp `json:"start_at"`
+	EndAt          *Timestamp `json:"end_at"`
+	AssigneeUserID uuid.UUID  `json:"assignee_user_id"`
+	Title          string     `json:"title"`
+	Description    string     `json:"desc"`
+	StoryPoint     uint       `json:"story_point"`
 	// for tasks that this task depends on
 	DependsOnTaskIDs []uuid.UUID `json:"depends_on_task_ids"`
 	//for tasks that depend on this task
-	// DependentByTaskIDs []uuid.UUID `json:"dependent_by_task_ids"`
+	//DependentByTaskIDs []uuid.UUID `json:"dependent_by_task_ids"`
 	ParentID *uuid.UUID `json:"parent_id"`
 	// SubTasksIDs        []uuid.UUID `json:"sub_tasks"`
 }
@@ -38,11 +38,12 @@ func TaskToUserTask(t task.Task) UserTask {
 
 func UserTaskToTask(userTaskReq *UserTask, userID uuid.UUID) *task.Task {
 	return &task.Task{
-		Title:           userTaskReq.Title,
-		Description:     userTaskReq.Description,
-		StartAt:         time.Time(userTaskReq.StartAt),
-		EndAt:           time.Time(userTaskReq.EndAt),
+		Title:       userTaskReq.Title,
+		Description: userTaskReq.Description,
+		//StartAt:         time.Time(userTaskReq.StartAt),
+		//EndAt:           time.Time(userTaskReq.EndAt),
 		StoryPoint:      userTaskReq.StoryPoint,
+		BoardID:         userTaskReq.Board_id,
 		CreatedByUserID: userID, //
 		// BoardID: t.BoardID,
 		ParentID:         userTaskReq.ParentID,
