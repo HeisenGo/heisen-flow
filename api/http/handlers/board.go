df72e55b3447c87971b29880a652a9388ad68fcf
kiarashfz <kiarashfz.fazli@gmail.com>
2024-07-10 18:57:59 +0330

    feat: import changes

--- api/http/handlers/board.go
+++ api/http/handlers/board.go
@@ -53,13 +53,13 @@ func CreateUserBoard(serviceFactory ServiceFactory[*service.BoardService]) fiber
 		b.CreatedAt = time.Now()
 		if err := boardService.CreateBoard(c.UserContext(), b, ubr); err != nil {
 			if errors.Is(err, board.ErrWrongType) || errors.Is(err, board.ErrInvalidName) {
-				return BadRequest(c, err)
+				return presenter.BadRequest(c, err)
 			}
 
-			return InternalServerError(c, err)
+			return presenter.InternalServerError(c, err)
 		}
 
-		return Created(c, "Board created successfully", fiber.Map{
+		return presenter.Created(c, "Board created successfully", fiber.Map{
 			"board_id":        b.ID,
 			"user_board_role": ubr.ID,
 		})
@@ -89,10 +89,10 @@ func InviteToBoard(serviceFactory ServiceFactory[*service.BoardService]) fiber.H
 			// 	status = fiber.StatusBadRequest
 			// }
 
-			return BadRequest(c, err)
+			return presenter.BadRequest(c, err)
 		}
 
-		return OK(c, "User successfully invited", fiber.Map{
+		return presenter.OK(c, "User successfully invited", fiber.Map{
 			"role":               ubr.Role,
 			"user_board_role_id": ubr.ID,
 			"board_id":           ubr.BoardID,
