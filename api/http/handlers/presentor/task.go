3853839284b07b10351cfb837a9730912824cfb8
kiarashfz <kiarashfz.fazli@gmail.com>
2024-07-10 16:32:56 +0330

    feat: start at and end at added

--- api/http/handlers/presentor/task.go
+++ api/http/handlers/presentor/task.go
@@ -2,19 +2,20 @@ package presenter
 
 import (
 	"server/internal/task"
+	"time"
 
 	"github.com/google/uuid"
 )
 
 type UserTask struct {
-	ID             uuid.UUID  `json:"task_id"`
-	Board_id       uuid.UUID  `json:"board_id"`
-	StartAt        *Timestamp `json:"start_at"`
-	EndAt          *Timestamp `json:"end_at"`
-	AssigneeUserID uuid.UUID  `json:"assignee_user_id"`
-	Title          string     `json:"title"`
-	Description    string     `json:"desc"`
-	StoryPoint     uint       `json:"story_point"`
+	ID             uuid.UUID `json:"task_id"`
+	BoardID        uuid.UUID `json:"board_id"`
+	StartAt        Timestamp `json:"start_at"`
+	EndAt          Timestamp `json:"end_at"`
+	AssigneeUserID uuid.UUID `json:"assignee_user_id"`
+	Title          string    `json:"title"`
+	Description    string    `json:"desc"`
+	StoryPoint     uint      `json:"story_point"`
 	// for tasks that this task depends on
 	DependsOnTaskIDs []uuid.UUID `json:"depends_on_task_ids"`
 	//for tasks that depend on this task
@@ -38,12 +39,12 @@ func TaskToUserTask(t task.Task) UserTask {
 
 func UserTaskToTask(userTaskReq *UserTask, userID uuid.UUID) *task.Task {
 	return &task.Task{
-		Title:       userTaskReq.Title,
-		Description: userTaskReq.Description,
-		//StartAt:         time.Time(userTaskReq.StartAt),
-		//EndAt:           time.Time(userTaskReq.EndAt),
+		Title:           userTaskReq.Title,
+		Description:     userTaskReq.Description,
+		StartAt:         time.Time(userTaskReq.StartAt),
+		EndAt:           time.Time(userTaskReq.EndAt),
 		StoryPoint:      userTaskReq.StoryPoint,
-		BoardID:         userTaskReq.Board_id,
+		BoardID:         userTaskReq.BoardID,
 		CreatedByUserID: userID, //
 		// BoardID: t.BoardID,
 		ParentID:         userTaskReq.ParentID,
