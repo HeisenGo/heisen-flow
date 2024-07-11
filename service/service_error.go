124a9fb4d304dc00b2c77453190159ac63a14c95
Neda Hn <z.hosseini.7@gmail.com>
2024-07-12 00:59:47 +0330

    feat: service layer errors

--- service/service_error.go
+++ service/service_error.go
@@ -3,6 +3,7 @@ package service
 import "errors"
 
 var (
-	ErrPermissionDenied = errors.New("permission denied: cannot create task")
-	ErrNotMember = errors.New("the assignee is not a member of this board")
+	ErrPermissionDenied = errors.New("permission denied")
+	ErrNotMember        = errors.New("the assignee is not a member of this board")
+	ErrCantAssigned     = errors.New("assignee is a viewer")
 )
