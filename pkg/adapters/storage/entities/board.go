bea6a4e05ac4be04baf879e38f4f845027e25767
kiarashfz <kiarashfz.fazli@gmail.com>
2024-07-12 08:52:16 +0330

    feat: usable fileds added

--- pkg/adapters/storage/entities/board.go
+++ pkg/adapters/storage/entities/board.go
@@ -8,14 +8,15 @@ import (
 )
 
 type Board struct {
-	ID        uuid.UUID      `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
-	Name      string         `gorm:"index"`
+	ID        uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
+	Name      string    `gorm:"index"`
 	Type      string
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt gorm.DeletedAt `gorm:"index"`
 	// Relationships
-	//Users     []User `gorm:"many2many:user_board_roles;constraint:OnDelete:CASCADE;"`
-	Tasks      []Task `gorm:"foreignKey:BoardID"`
+	Users          []User          `gorm:"many2many:user_board_roles;constraint:OnDelete:CASCADE;"`
+	Tasks          []Task          `gorm:"foreignKey:BoardID"`
 	UserBoardRoles []UserBoardRole `gorm:"foreignKey:BoardID"`
+	Columns        []Column        `gorm:"foreignKey:BoardID"`
 }
