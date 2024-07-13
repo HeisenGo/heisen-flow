0514d7b56c1933fc8aae7ed86060ff4d29c60708
Neda Hn <z.hosseini.7@gmail.com>
2024-07-13 18:25:08 +0330

    feat: ops for get notif by id

--- internal/notification/ops.go
+++ internal/notification/ops.go
@@ -14,23 +14,28 @@ func NewOps(repo Repo) *Ops {
 	return &Ops{repo}
 }
 
-func (o *Ops) CreateNotification(ctx context.Context,notif *Notification) error {
+func (o *Ops) CreateNotification(ctx context.Context, notif *Notification) error {
 	err := o.repo.CreateNotification(ctx, notif)
 	return err
 }
 
-func (o *Ops) GetUserUnseenNotifications(ctx context.Context, userID uuid.UUID) ([]Notification, error){
-	notification , err := o.repo.GetUserUnseenNotifications(ctx,userID)
-	if err != nil{
-		return nil , err
+func (o *Ops) GetUserUnseenNotifications(ctx context.Context, userID uuid.UUID) ([]Notification, error) {
+	notification, err := o.repo.GetUserUnseenNotifications(ctx, userID)
+	if err != nil {
+		return nil, err
 	}
 	return notification, nil
 }
 
-func (o *Ops) MarkNotificationAsSeen(ctx context.Context, notificationID uuid.UUID) error{
-	err := o.repo.MarkNotificationAsSeen(ctx,notificationID)
+func (o *Ops) MarkNotificationAsSeen(ctx context.Context, notificationID uuid.UUID) (*Notification, error) {
+	notif, err := o.repo.MarkNotificationAsSeen(ctx, notificationID)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	return nil
+	return notif, nil
+}
+
+
+func (o *Ops)GetNotificationByID(ctx context.Context, notificationID uuid.UUID)(*Notification, error){
+	return o.repo.GetNotificationByID(ctx, notificationID)
 }
