33d3f903eba059674255b65bc66f291b93dcc442
Neda Hn <z.hosseini.7@gmail.com>
2024-07-13 20:04:04 +0330

    fix: notification errors handled

--- api/http/handlers/notification.go
+++ api/http/handlers/notification.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"errors"
 	presenter "server/api/http/handlers/presentor"
+	"server/internal/notification"
 	"server/internal/user"
 	"server/pkg/jwt"
 	"server/service"
@@ -19,11 +20,10 @@ func GetNotifications(notificationService *service.NotificationService) fiber.Ha
 		}
 		notifList, err := notificationService.GetUserNotifications(c.UserContext(), userClaims.UserID)
 		if err != nil {
-			status := fiber.StatusInternalServerError
-			if errors.Is(err, user.ErrUserNotFound) {
-				status = fiber.StatusBadRequest
+			if errors.Is(err, user.ErrUserNotFound) || errors.Is(err, notification.ErrNotifsNotFound) {
+				return presenter.BadRequest(c, err)
 			}
-			return SendError(c, err, status)
+			return presenter.InternalServerError(c, err)
 		}
 		res := presenter.BatchNotifToNotifResp(notifList)
 		return presenter.OK(c, "notifications successfully fetched", res)
@@ -36,7 +36,7 @@ func UpdateNotifications(notificationService *service.NotificationService) fiber
 		if !ok {
 			return SendError(c, errWrongClaimType, fiber.StatusBadRequest)
 		}
-		
+
 		notificationIDParam := c.Params("notifID")
 		notificationID, err := uuid.Parse(notificationIDParam)
 		if err != nil {
@@ -44,11 +44,10 @@ func UpdateNotifications(notificationService *service.NotificationService) fiber
 		}
 		n, err := notificationService.MarkNotificationAsSeen(c.UserContext(), notificationID, userClaims.UserID)
 		if err != nil {
-			status := fiber.StatusInternalServerError
-			if errors.Is(err, user.ErrUserNotFound) {
-				status = fiber.StatusBadRequest
+			if errors.Is(err, user.ErrUserNotFound) || errors.Is(err, notification.ErrNotifNotFound) {
+				return presenter.BadRequest(c, err)
 			}
-			return SendError(c, err, status)
+			return presenter.InternalServerError(c, err)
 		}
 		res := presenter.DomainNotifToNotifResp(*n)
 		return presenter.OK(c, "Marked As Seen", res)
