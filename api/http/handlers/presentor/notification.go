40bd7bdd7ccc4f961645bcc1e7ed34f23f07bea5
Neda Hn <z.hosseini.7@gmail.com>
2024-07-13 19:29:05 +0330

    fix: typos

--- api/http/handlers/presentor/notification.go
+++ api/http/handlers/presentor/notification.go
@@ -9,24 +9,8 @@ import (
 	"github.com/google/uuid"
 )
 
-type NotificationReq struct {
-	UserBoardRoleID uuid.UUID `json:"user_board_role_id"`
-	Description     string    `json:"description"`
-	IsSeen          bool      `json:"is_seen"`
-	CreatedAt       time.Time
-	UpdatedAt       time.Time
-}
-
-func NotificationToNotificationDomain(not *NotificationReq) *notification.Notification {
-	return &notification.Notification{
-		UserBoardRoleID: not.UserBoardRoleID,
-		Description:     not.Description,
-		IsSeen:          not.IsSeen,
-	}
-}
-
 type NotifResp struct {
-	CreatedAt        time.Time                     `json:"created_ta"`
+	CreatedAt        time.Time                     `json:"created_at"`
 	ID               uuid.UUID                     `json:"id"`
 	IsSeen           bool                          `json:"is_seen"`
 	Description      string                        `json:"desc"`
